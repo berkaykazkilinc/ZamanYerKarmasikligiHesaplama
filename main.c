45efc87326771929677809b0e0a98cb300b07e88
berkaykazkilinc <75437423+berkaykazkilinc@users.noreply.github.com>
2022-03-11 11:58:03 +0300

    guncelleme
    fscanf yerine fgets kullanmamız daha faydalı olacak,dosya kontrol fonksiyonunu da yazdım ve projeye ekledim

--- main.c
+++ main.c
@@ -6,16 +6,19 @@
 
 void ZamanKarmasikligi();
 void YerKarmasikligi();
+void DosyaKontrol();
 
 int main()
 {
-    ZamanKarmasikligi();
-
+    //ZamanKarmasikligi();
+    //YerKarmasikligi();
+    DosyaKontrol();
 
     return 0;
 }
-void ZamanKarmasikligi(){
- char deneme[50];
+void ZamanKarmasikligi()
+{
+    char deneme[255];
     int n=0;
     FILE *dosya;
     fpos_t pos;
@@ -26,18 +29,18 @@ void ZamanKarmasikligi(){
     }
     while(!feof(dosya))
     {
-       fscanf(dosya,"%s",&deneme);
-       //printf("%s ",deneme);
-    if(strstr(deneme,"for(")!=NULL)
-    {
-        n+=1;
-        fgetpos(dosya,&pos);
-        printf("Dosyanin aktif konumu: %ld\n", pos);
-    }
-    if(strstr(deneme,"while(")!=NULL)
-    {
-        n+=1;
-    }
+        fscanf(dosya,"%s",&deneme);
+        //printf("%s ",deneme);
+        if(strstr(deneme,"for(")!=NULL)
+        {
+            n+=1;
+            fgetpos(dosya,&pos);
+            printf("Dosyanin aktif konumu: %ld\n", pos);
+        }
+        if(strstr(deneme,"while(")!=NULL)
+        {
+            n+=1;
+        }
     }
     /*if(strcmp(deneme,"for")==0)
     {
@@ -47,11 +50,29 @@ void ZamanKarmasikligi(){
     {
         printf("Big-O(n) = O(n^%d)\n",n);
     }
-    printf("%s",deneme);
+    printf("%s\n",deneme);
+    int x=0,j=0,z=0;
+    x=strlen(deneme);
+    for(j=0; j<x-1; j++)
+    {
+        if(deneme[j]==',')
+        {
+            z++;
+        }
+        printf("|||||%d",z);
+        printf("%c\n",deneme[0]);
+
+    }
+
+
     fclose(dosya);
 }
-/*void YerKarmasikligi(){
+void YerKarmasikligi()
+{
     FILE *dosya;
+    char deneme[255];
+    int string_boyutu=0;
+    int yer_karmasikligi=0;
     if((dosya=fopen(TEXT_DOSYA,"r"))== NULL)
     {
         printf("Dosya Acilamadi");
@@ -59,10 +80,48 @@ void ZamanKarmasikligi(){
     }
     while(!feof(dosya))
     {
-       fscanf(dosya,"%s",&deneme);
-       if(strstr(deneme,"int")!=NULL)
-       {
+        fgets(deneme,255,dosya);
+        if(strstr(deneme,"int")!=NULL)
+        {
+            /*string_boyutu=strlen(deneme);
+            int i=0;
+            for(i=0;i<string_boyutu;i++)
+            {
+                if(deneme[i]==',');
+                {
+                    //yer_karmasikligi++;
+                }
+                printf("%c",deneme[i]);
+            }*/
+            yer_karmasikligi+=1;
+            // yer_karmasikligi=yer_karmasikligi*4;
 
-       }
+
+        }
     }
-}*/
+    yer_karmasikligi=yer_karmasikligi*4;
+    printf("%d",yer_karmasikligi);
+}
+void DosyaKontrol()
+{
+    FILE *dosya;
+    char deneme[255];
+    int kontrol=0;
+    if((dosya=fopen(TEXT_DOSYA,"r"))== NULL)
+    {
+        printf("Dosya Acilamadi");
+        exit(1);
+    }
+    fgets(deneme,255,dosya);
+    kontrol=strlen(deneme);
+    printf("%d",kontrol);
+    if(kontrol==0)
+    {
+        printf("Dosya Icerigi Bos");
+    }
+    else
+    {
+        printf("Dosya Icerigi Mevcut");
+    }
+
+}
